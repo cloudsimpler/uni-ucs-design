7f81b64ab41edb04d1466deff695eb111f9e182a
dengqichang <deng.qi.chang@qq.com>
2024-09-05 01:17:14 +0800

    初始化

---
+++ uni_modules/ucs-svg/components/ucs-svg/ucs-svg.vue
@@ -0,0 +1,44 @@
+<template>
+	<!-- #ifdef APP-PLUS-NVUE -->
+	<ucs-uts-svg :style="{width:`${width}${unit}`,height:`${height}${unit}`}" :src="src" />
+	<!-- #endif -->
+	<!-- #ifndef APP-PLUS-NVUE -->
+	<image :src="`data:image/svg+xml;charset=utf-8,${encodeURIComponent(src)}`" :style="{width:`${width}${unit}`,height:`${height}${unit}`}" />
+	<!-- #endif -->
+</template>
+
+<script>
+	/**
+	 * ucs-svg svg组件
+	 * @description 一款适用于 uni-app / uni-app-x 的 SVG 组件，全端全版本适配。
+	 * @tutorial 
+	 * @property {String} src svg资源参数
+	 * @property {Number} width 宽度
+	 * @property {Number} height 高度
+	 * @property {String} unit 单位，默认px
+	 */
+	export default {
+		props: {
+			src: {
+				type: String,
+				default: ""
+			},
+			width: {
+				type: Number,
+				default: 24
+			},
+			height: {
+				type: Number,
+				default: 24
+			},
+			unit: {
+				type: String,
+				default: "px"
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
