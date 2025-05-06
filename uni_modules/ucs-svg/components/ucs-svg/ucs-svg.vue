908b6190bc7153388a92dafad46ce99655b37d64
邓琪昌 <deng.qi.chang@qq.com>
2025-05-06 17:35:04 +0800

    兼容鸿蒙版本，取消nvue适配

--- uni_modules/ucs-svg/components/ucs-svg/ucs-svg.vue
+++ uni_modules/ucs-svg/components/ucs-svg/ucs-svg.vue
@@ -1,7 +1,7 @@
 <template>
 	<!-- #ifdef UNI-APP-X -->
 	<!-- #ifdef APP -->
-	<ucs-uts-svg :style="{width:`${width}${unit}`,height:`${height}${unit}`}" :src="src" />
+	<native-view :style="{width:`${width}${unit}`,height:`${height}${unit}`}" @init="onInit" />
 	<!-- #endif -->
 	<!-- #ifndef APP -->
 	<view class="__ucs-svg">
@@ -11,19 +11,14 @@
 	<!-- #endif -->
 	<!-- #endif -->
 	<!-- #ifndef UNI-APP-X -->
-	<!-- #ifdef APP-PLUS-NVUE -->
-	<ucs-uts-svg :style="{width:`${width}${unit}`,height:`${height}${unit}`}" :src="src" />
-	<!-- #endif -->
-	<!-- #ifndef APP-PLUS-NVUE -->
 	<view class="__ucs-svg">
 		<image :src="`data:image/svg+xml;charset=utf-8,${encodeURIComponent(src)}`"
 			:style="{width:`${width}${unit}`,height:`${height}${unit}`}" />
 	</view>
 	<!-- #endif -->
-	<!-- #endif -->
 </template>
 
-<script>
+<script setup lang="uts">
 	/**
 	 * ucs-svg svg组件
 	 * @description 一款适用于 uni-app / uni-app-x 的 SVG 组件，全端全版本适配。
@@ -33,26 +28,50 @@
 	 * @property {Number} height 高度
 	 * @property {String} unit 单位，默认px
 	 */
-	export default {
-		props: {
-			src: {
-				type: String,
-				default: ""
-			},
-			width: {
-				type: Number,
-				default: 24
-			},
-			height: {
-				type: Number,
-				default: 24
-			},
-			unit: {
-				type: String,
-				default: "px"
-			}
+	const props = defineProps({
+		src: {
+			type: String,
+			default: ""
+		},
+		width: {
+			type: Number,
+			default: 60
+		},
+		height: {
+			type: Number,
+			default: 60
+		},
+		unit: {
+			type: String,
+			default: "px"
 		}
+	})
+
+	// #ifdef UNI-APP-X
+	// #ifdef APP
+	import { UcsSvg } from '@/uni_modules/ucs-svg';
+	const svg = ref<UcsSvg | null>(null)
+
+	function onInit(e : UniNativeViewInitEvent) {
+		svg.value = new UcsSvg(e.detail.element)
+		// #ifdef APP-HARMONY
+		svg.value?.setSource(props.src)
+		// #endif
+		// #ifndef APP-HARMONY
+		svg.value?.setSource(props.src)
+		// #endif
 	}
+
+	watch(() : string => props.src, () => {
+		// #ifdef APP-HARMONY
+		svg.value?.setSource(props.src)
+		// #endif
+		// #ifndef APP-HARMONY
+		svg.value?.setSource(props.src)
+		// #endif
+	}, { immediate: true })
+	// #endif
+	// #endif
 </script>
 
 <style scoped>
